43ccd326b924e618b945fe83d593bdd72e6b0af9
Trent Guillory <trentmichaelguillory@gmail.com>
2020-10-01 17:46:00 -0500

    Initial Commit

---
+++ HomeTheme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeTheme
+//
+//  Created by Trent Guillory on 10/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeTheme/HomeThemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeThemeApp.swift
+//  HomeTheme
+//
+//  Created by Trent Guillory on 10/1/20.
+//
+
+import SwiftUI
+
+@main
+struct HomeThemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
